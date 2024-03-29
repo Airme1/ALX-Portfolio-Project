26eda462cfc94f3aa97fa04c5483f687f5a4da48
Airme1 <emmanuelochoja@yahoo.com>
2024-03-29 18:13:31 +0100

    Beginner Passing Drills Fetch

--- eagles-world-cup-2041/src/layouts/beginner/BeginnerPass.jsx
+++ eagles-world-cup-2041/src/layouts/beginner/BeginnerPass.jsx
@@ -1,7 +1,46 @@
-export default function BeginnerPass() {
-    return (
-        <>
-            <h1>Beginners Passing Drill</h1>
-        </>
-    )
+import React, { useState, useEffect } from "react";
+import VideoList from '../VideoList';
+
+function BeginnerPass() {
+  const [videos, setVideos] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://www.googleapis.com/youtube/v3/search?key=AIzaSyBJTVJnlWww2K6Eily-GxJQVQuNeHO_7Tw&q=Beginner soccer passing drill&type=video&part=snippet&maxResults=12"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const videosData = await response.json();
+        setVideos(videosData.items);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this effect will only run once after the initial render
+
+  if (loading) {
+    return <div>Beginner Passing Drills Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  
+  return (
+    <div>
+      {/* Render your fetched data here */}
+      <VideoList videos={videos} />
+    </div>
+  );
 }
+
+export default BeginnerPass;
